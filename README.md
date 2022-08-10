b6911ca399951ce24b15874db56a9ccc6ae6680a
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-08-10 15:23:28 +0530

    modified the k8s/manifest files and as well package name

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spb-docker
+#testing this spring boot application via tekton pipeline
--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -1,4 +1,4 @@
-package com.mkyong;
+package com.amos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
