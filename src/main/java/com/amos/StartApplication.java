e57fc3e150e7983ff1dc0b96217ee54e498afbc5
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-11-22 16:13:47 +0530

    not simple with webhook

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "Tekton *** Pipeline !!! ");
+        model.addAttribute("msg", "it's not possbile to setup the webhook in tetkton");
         
         return "index";
     }
