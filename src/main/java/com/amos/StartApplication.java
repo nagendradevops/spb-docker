d9ec514108286ce33afb9f8f5b87caa101061dd8
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-12-07 15:56:25 +0530

    updated the piperun with worksapcd

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "reconfigured the listener, changed");
+        model.addAttribute("msg", "reconfigured the listener, given pvc to pipelinerun");
         
         return "index";
     }
