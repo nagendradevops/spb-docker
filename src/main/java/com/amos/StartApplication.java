084555768befa8460e84f9167847d9b7058fc3d9
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-12-07 14:32:59 +0530

    updated hte code

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "testing with new webhook");
+        model.addAttribute("msg", "Reconfigured the triggers & webhook");
         
         return "index";
     }
