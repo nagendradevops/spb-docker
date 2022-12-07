cdccc8e19bc7ce6b0881e57591fc228c323115bf
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-12-07 16:05:15 +0530

    updated the giturl in trigertemplate

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "reconfigured the listener, given pvc to pipelinerun");
+        model.addAttribute("msg", "updated the right git URL, given pvc to pipelinerun");
         
         return "index";
     }
