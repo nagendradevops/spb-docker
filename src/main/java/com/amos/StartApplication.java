7b3faa3f96ac386c18bcf47d51774d2785a7a533
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-12-07 15:41:12 +0530

    removed & reconfigured hte listeners

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "reconfigured the listener, secrect has changed");
+        model.addAttribute("msg", "reconfigured the listener, changed");
         
         return "index";
     }
