bfa873072c5bf51ea63319dc0af6bb5344ce792c
Nagendra Reddy Bandi <nagendra-reddy.bandi@atos.net>
2022-12-29 17:25:10 +0530

    testing

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Tekton + Spring Boot + Docker");
-        model.addAttribute("msg", "We have done automation for Tekton Pipelines & webhook triggers!!!");
+        model.addAttribute("msg", "We are doing automation for Tekton Pipelines & webhook triggers!!!");
         
         return "index";
     }
